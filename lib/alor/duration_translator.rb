2fb9334dc829cc823e710eee8b14d0aea28e2173
Aaron Collier <aaron.collier@stanford.edu>
2024-09-17 21:14:24 -0700

    Refactor DurationTranslator

--- lib/alor/duration_translator.rb
+++ lib/alor/duration_translator.rb
@@ -13,9 +13,7 @@ module Alor
     def translate
       return '00:00:00' if @duration.nil? || @duration == 'P0D'
 
-      hours, minutes, seconds = @duration.match(/PT(\d+H)?(\d+M)?(\d+S)?/).captures.map { |x| x.to_i }
-
-      "#{hours.to_s.rjust(2, '0')}:#{minutes.to_s.rjust(2, '0')}:#{seconds.to_s.rjust(2, '0')}"
+      @duration.match(/PT(\d+H)?(\d+M)?(\d+S)?/).captures.map { |x| x.to_i.to_s.rjust(2, '0') }.join(':')
     end
   end
 end
